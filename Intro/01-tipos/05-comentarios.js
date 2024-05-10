4055a8571316355383408370af9c06fc803f7e59
ricardofm09 <rf01527@gmail.com>
2024-05-09 20:43:32 -0600

    Comentarios
    Con slash y asteriscos

---
+++ Intro/01-tipos/05-comentarios.js
@@ -0,0 +1,15 @@
+// 
+/*
+Todo lo que escribamos entre en medio de los slash
+y asteriscos es ignorado
+*/
+
+/**
+ * Este es un comentario
+ * de muchas
+ * lineas
+ */
+
+let numero = 42; //Comentario
+
+console.log(numero);
