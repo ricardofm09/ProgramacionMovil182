9075a39ab6332c5dd5e67b2dc9c8a0ff80bcd527
ricardofm09 <rf01527@gmail.com>
2024-05-09 23:03:21 -0600

    Operadores de comparacion
    Se utilizan para poder comparar distintas variables entre si u otra cosa

---
+++ Intro/02-operadores/03-comparacion.js
@@ -0,0 +1,12 @@
+let a = 10;
+//reglacionales
+console.log(a > 5);
+console.log(a >= 5);
+console.log(a < 5);
+console.log(a <= 5);
+
+//igualdad
+console.log(a == 10);
+console.log(a != 10);
+console.log(a === '10');
+console.log(a !== '10');
