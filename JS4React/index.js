38493d19cbdb96a686ec4c7c7ecb1badd9cc0b97
ricardofm09 <rf01527@gmail.com>
2024-05-20 15:41:53 -0600

    AvancePract2

---
+++ JS4React/index.js
@@ -0,0 +1,38 @@
+//console.log('hola')
+//Function
+/*
+function suma(a,b){
+    return a+b
+}
+
+suma(2,3);
+
+//VERSION NO SIMPLIFICADA
+console.log(suma(2,3));
+*/
+/*
+const suma = (a,b)=>{
+    return a+ b
+}
+
+console.log(suma(2,3));
+*/
+
+//VERSION SIMPLIFICADA
+/*const suma=(a,b)=> a+b
+
+console.log(suma(2,3));
+*/
+
+const usuario = {
+    nombre:'Ricardo',
+    apellido:'Flores',
+    edad: 20,
+    direccion:{
+        pais:'Mexico',
+        ciudad:'Queretaro',
+        calle:'5 de febrero'
+    },
+    amigos:['Alex','Carlos'],
+    estatus: true,
+}
