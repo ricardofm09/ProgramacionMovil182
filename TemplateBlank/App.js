2177dae954bc1a82e121921a88c2410cdd8475d1
ricardofm09 <rf01527@gmail.com>
2024-06-21 16:20:46 -0600

    image
    image

--- TemplateBlank/App.js
+++ TemplateBlank/App.js
@@ -1,20 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ImageBackground } from 'react-native';
 
 export default function App() {
+  const[name, setName] = useState('');
+  const[email, setEmail] = useState('');
+  const[password, setPassword] = useState('');
+  const[submit, setSubmit] = useState('');
+
+  const Nombre = (text) => { setName(text); }
+  const Email = (text) => { setEmail(text); }
+  const Password = (text) => { setPassword(text); }
+
+  const Guardar = () => { alert(`Nombre: ${name}\nEmail: ${email}\nPassword: ${password}`); }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={require('./background.jpg')} style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <View style={styles.contentContainer}>
+          <View style={[styles.boxContainer, styles.widthContainer]}>
+            <Text style={[styles.text]}> Nombre {submit} </Text>
+            <TextInput style={[styles.input, styles.text]} placeholder='Nombre' onChangeText={Nombre} value={name} />
+            <Text style={[styles.text]}> Email {submit} </Text>
+            <TextInput style={[styles.input, styles.text]} placeholder='Email' onChangeText={Email} value={email} />
+            <Text style={[styles.text]}> Password {submit} </Text>
+            <TextInput style={[styles.input, styles.text]} placeholder='Contraseña' onChangeText={Password} value={password} secureTextEntry={true} />
+            <Button title='Guardar' onPress={Guardar} />
+          </View>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  contentContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  boxContainer: {
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    padding: 20,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+  },
+  widthContainer: {
+    width: '200%',
+  },
+  input: {
+    width: '100%',
+    height: 50,
+    borderBottomWidth: 2,
+    borderBottomColor: '#CF87EA',
+    paddingHorizontal: 10,
+  },
+  text: {
+    fontSize: 18,
+    marginVertical: 10,
   },
 });
